a544b62c7fed4a2d804e6b51aaf2248942e1e9d1
supun <supun.gamage@ebuilder.com>
2017-03-03 18:57:56 +0530

    todolist and todo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,26 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'clean the gardern'
+        }
+      ]
+    };
+  },
   render: function() {
+    var {todos} = this.state;
     return(
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     );
   }
---
+++ app/components/TodoList.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+  render: function() {
+    var {todos} = this.props;
+
+    /*function renderTodos1() {
+      return todos.map(function(todo){
+        return(
+          <Todo key={todo.id}/>
+        );
+      });
+    }*/
+
+    var renderTodos = () => {
+      return todos.map((todo) => {
+        return(
+          <Todo key={todo.id} {...todo}/>
+        );
+      });
+    }
+
+    return(
+      <div>
+        {renderTodos()}
+      </div>
+    );
+  }
+});
+
+module.exports = TodoList;
