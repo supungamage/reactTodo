fbefce02f492109465afa86c0d123cbb9cb19c2d
supun <supun.gamage@ebuilder.com>
2017-03-09 14:51:35 +0530

    save to localStorage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,27 +1,22 @@
 var React = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-var uuid = require('node-uuid');
+var TodoAPI = require('TodoAPI');
+
 
 var TodoApp = React.createClass({
   getInitialState: function() {
     return {
       showCompleated: false,
       searchText: '',
-      todos: [
-        {
-          id: uuid(),
-          text: 'Walk the dog',
-          compleated: true
-        },
-        {
-          id: uuid(),
-          text: 'clean the gardern',
-          compleated: false
-        }
-      ]
+      todos: TodoAPI.getTodos()
     };
+  },
+  componentDidUpdate: function() {
+    TodoAPI.setTodos(this.state.todos);
   },
   handleAddTodo: function(text) {
     this.setState({
