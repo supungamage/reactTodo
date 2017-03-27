11865aa39ae8e4f8604d394912dcc2292c5a58b1
supun <supun.gamage@ebuilder.com>
2017-03-27 16:56:26 +0530

    added reducers

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,29 @@
+var expect = require('expect');
+var df = require('deep-freeze-strict');
+
+var reducers = require('reducers');
+
+describe('Reducers', () => {
+  describe('searchTextReducer', () => {
+    it('Should set searchText', () => {
+      var action = {
+        type: 'SET_SEARCH_TEXT',
+        searchText: 'some text'
+      };
+      var res = reducers.searchTextReducer(df(''), df(action));
+
+      expect(res).toEqual(action.searchText);
+    });
+  });
+
+  describe('showCompletedReducer', () => {
+    it('Should flipp showCompleted status', () => {
+      var action = {
+        type: 'TOGGLE_SHOW_COMPLETED'
+      };
+      var res = reducers.showCompletedReducer(df(false), df(action));
+
+      expect(res).toEqual(true);
+    });
+  });
+});
